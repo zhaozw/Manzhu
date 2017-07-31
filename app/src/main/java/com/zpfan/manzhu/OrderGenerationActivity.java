b75bd0402293b7676caed081bdedb5f61bcffdaf
Z-chao <zanchao194@gmail.com>
2017-07-31 23:13:45 +0800

    下单完成后的界面搭建完成

--- app/src/main/java/com/zpfan/manzhu/OrderGenerationActivity.java
+++ app/src/main/java/com/zpfan/manzhu/OrderGenerationActivity.java
@@ -3,8 +3,18 @@ package com.zpfan.manzhu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.gson.reflect.TypeToken;
 import com.zpfan.manzhu.bean.AvatorBean;
+import com.zpfan.manzhu.utils.Utils;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class OrderGenerationActivity extends AppCompatActivity {
 
@@ -28,13 +38,47 @@ public class OrderGenerationActivity extends AppCompatActivity {
         } else if (type.equals("server")) {
             //服务的界面
 
-
+        }
+        //发送请求去获取订单的详情
+        if (avator.isRet()) {
+            getorderDetil(avator.getRetmsg());
         }
 
 
 
 
 
+
+    }
+
+    private void getorderDetil(String retmsg) {
+        Call<String> details = Aplication.mIinterface.getOrderDetails(retmsg);
+
+        details.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Log.i("zc", "onResponse:   看看数据"  + call.request().toString());
+                String body = response.body();
+
+                if (body != null) {
+                    Type type = new TypeToken<ArrayList<AvatorBean>>() {
+                    }.getType();
+
+                    ArrayList<AvatorBean> avatorBeen = Utils.gson.fromJson(body, type);
+
+
+
+                }
+
+
+
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+
+            }
+        });
 
     }
 }
